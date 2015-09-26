e94fb04ed10b20b2a1b87bb35d5c5879ce905bfb
glovell <glovell3@gmail.com>
2015-09-26 13:28:04 -0400

    Edited homework template file with created functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The following code creates an enhanced matrix object that stores
+## the value of a square matrix and its inverse if the inverse has been 
+## computed.  This enhanced matrix object is created by evaluating the 
+## function makeCacheMatrix.  The value of the matrix inverse
+## can be computed and set in this object by the function cacheSolve.
+## cacheSolve will modify this enhanced matrix object if and only if
+## this object does not already have the value of the matrix inverse.
 
+## The function makeCacheMatrix outputs an enhanced matrix object based 
+## on the square matrix x.
 makeCacheMatrix <- function(x = matrix()) {
-
+  minv <- NULL
+  set <- function(y) {
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) minv <<- inv
+  getinv <- function() minv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
-
-
-## Write a short comment describing this function
-
+## This function cacheSolve takes as input the special matrix object created by
+## evaluating the function makeCacheMatrix.  It modifies this object
+## if necessary by setting the value of the matrix inverse if this value has
+## not been cached.  This function also returns the computed or cached 
+## value of the inverse. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  minv <- x$getinv()
+  if(!is.null(minv)) {
+    message("getting cached data")
+    return(minv)
+  }
 }
